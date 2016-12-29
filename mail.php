e61e447c5d58769661a09eb3246d1ccf9edaad02
Ogo Nwodoh <ocn2000@barnard.edu>
2016-12-29 14:30:23 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,8 @@
 <?php
 	$to= "ogogurl@gmail.com";
 	$subject="Moodboard: What's your mood?";
-	$msg="Hi there from Moodboard! Please respond with a message, ranging from a word to a biography on what your current mood is.";
+	$msg="Hi there from Moodboard! Please respond with a message, ranging from a word to a biography on what your current mood is.
+	<strong> When you're finished with your update please add the tag: <EOM>, so that your message is correctly parsed. </strong>";
 	$msg=wordwrap($msg,70);
 	$headers= "From: webmaster@ogonwodoh.com" . "\r\n";
 	
