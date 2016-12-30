d116c04b2eb264b162e657f5790e63817f18127b
Ogo Nwodoh <ocn2000@barnard.edu>
2016-12-30 02:31:17 -0500

    Update mail_action.php

--- mail_action.php
+++ mail_action.php
@@ -1,8 +1,6 @@
 <?php
-//This is used to retreive a user's status updates and post it to the blog
-
 // open IMAP connection
-$mail = imap_open('{mail.ogonwodoh.com:143/novalidate-cert}INBOX',   'webmaster@ogonwodoh.com', '<password>') or die('Cannot connect: ' . imap_last_error());
+$mail = imap_open('{mail.ogonwodoh.com:143/novalidate-cert}INBOX',   'webmaster@ogonwodoh.com', 'Green2014') or die('Cannot connect: ' . imap_last_error());
 
 
 // grab a list of all the mail headers
@@ -16,14 +14,12 @@ $header = imap_header($mail, $last);
 // grab the body for the same message
 $structure = imap_fetchstructure($mail, $last);
 
+
 $body = imap_body($mail, $last);
+$body=imap_fetchbody($mail,$last, 1);
+$msg=explode("<END>",$body);//this tag will help us parse email responses
 
-$body=imap_fetchbody($mail,$last,1);
 
-//For now I am using the tag EOM to detect when a user has finished writing their status update. This makes it easier to parse the response
-$msg=strtok($body, "<EOM>");
-
-print_r($msg);
 
 // close the connection
 imap_close($mail);
